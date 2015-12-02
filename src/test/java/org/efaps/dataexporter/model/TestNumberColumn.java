250b5fd176cf92da082d71f94d31dbb56e40ac82
Jan Moxter <jan.moxter@innobix.com>
2015-12-02 17:41:14 -0500

    - Issue #2: Change to TestNG and make the tests work
    remoe junit

--- src/test/java/org/efaps/dataexporter/model/TestNumberColumn.java
+++ src/test/java/org/efaps/dataexporter/model/TestNumberColumn.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,46 +19,45 @@
  */
 package org.efaps.dataexporter.model;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.StringWriter;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-import org.efaps.dataexporter.model.NumberColumn;
 import org.efaps.dataexporter.output.text.TextExporter;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.testng.annotations.Test;
 
 public class TestNumberColumn {
-    
+
     @Test
     public void testNumberColumn() throws Exception {
-        StringWriter sw = new StringWriter();
-        
+        final StringWriter sw = new StringWriter();
+
         TextExporter exporter = new TextExporter(sw);
         exporter.getOptions().setPrintHeaders(false);
         exporter.addColumns(new NumberColumn("number", 10, 10));
         exporter.addRow((float)22/7);
         exporter.finishExporting();
         assertEquals("3.1428570747", sw.toString().trim());
-        
+
         exporter = new TextExporter(sw);
         exporter.getOptions().setPrintHeaders(false);
         exporter.addColumns(new NumberColumn("number", 10, 10));
         exporter.addRow((float)22/7);
         exporter.finishExporting();
     }
-    
+
     @Test
     public void testNumberDecimalFormatter() throws Exception {
-        StringWriter sw = new StringWriter();
-        
-        TextExporter exporter = new TextExporter(sw);
+        final StringWriter sw = new StringWriter();
+
+        final TextExporter exporter = new TextExporter(sw);
         exporter.getOptions().setPrintHeaders(false);
 
         exporter.addColumns(new NumberColumn("number", 10, NumberFormat.getNumberInstance(Locale.GERMAN)));
         exporter.addRow(1234567.234);
         exporter.finishExporting();
         assertEquals("1.234.567,234", sw.toString().trim());
-	}
+    }
 }
