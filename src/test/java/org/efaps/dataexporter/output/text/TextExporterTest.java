14b848b9d13cb62acf84064440810a7e849d9ff6
Jan Moxter <jan.moxter@innobix.com>
2015-12-02 17:19:49 -0500

    - Issue #2: Change to TestNG and make the tests work
    basic test

--- src/test/java/org/efaps/dataexporter/output/text/TextExporterTest.java
+++ src/test/java/org/efaps/dataexporter/output/text/TextExporterTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,21 +20,38 @@
 package org.efaps.dataexporter.output.text;
 
 import org.efaps.dataexporter.DataExporterTestBase;
-import org.efaps.dataexporter.output.text.TextExporter;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-public class TextExporterTest extends DataExporterTestBase {
-    
-    public TextExporterTest() {
-        exporter = new TextExporter(sw);
+/**
+ * The Class TextExporterTest.
+ *
+ * @author The eFaps Team
+ */
+public class TextExporterTest
+    extends DataExporterTestBase
+{
+
+    /**
+     * Instantiates a new text exporter test.
+     */
+    public TextExporterTest()
+    {
+        this.exporter = new TextExporter(this.sw);
     }
-    
+
+    /**
+     * Test basic.
+     *
+     * @throws Exception the exception
+     */
     @Test
-    public void testBasic() throws Exception {
+    public void testBasic()
+        throws Exception
+    {
         addData();
-        exporter.finishExporting();
-        
-        System.out.println(sw);
-        compareText("testBasic.txt", sw.toString());
+        this.exporter.finishExporting();
+
+        System.out.println(this.sw);
+        compareText("testBasic.txt", this.sw.toString());
     }
 }
