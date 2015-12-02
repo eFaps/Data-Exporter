ce0936f0be747dbad2ac88405a608de6efbc8774
Jan Moxter <jan.moxter@innobix.com>
2015-12-02 17:38:19 -0500

    - Issue #2: Change to TestNG and make the tests work
    wiki test

--- src/test/java/org/efaps/dataexporter/output/wiki/WikiExporterTest.java
+++ src/test/java/org/efaps/dataexporter/output/wiki/WikiExporterTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,21 +20,25 @@
 package org.efaps.dataexporter.output.wiki;
 
 import org.efaps.dataexporter.DataExporterTestBase;
-import org.efaps.dataexporter.output.wiki.WikiExporter;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-public class WikiExporterTest extends DataExporterTestBase {
-    
-    public WikiExporterTest() {
-        exporter = new WikiExporter(sw);
+public class WikiExporterTest
+    extends DataExporterTestBase
+{
+
+    public WikiExporterTest()
+    {
+        this.exporter = new WikiExporter(this.sw);
     }
-    
+
     @Test
-    public void testBasic() throws Exception {
+    public void testBasic()
+        throws Exception
+    {
         addData();
-        exporter.finishExporting();
-        
-        System.out.println(sw);
-        compareText("testBasic.txt", sw.toString());
+        this.exporter.finishExporting();
+
+        System.out.println(this.sw);
+        compareText("testBasic.txt", this.sw.toString());
     }
 }
